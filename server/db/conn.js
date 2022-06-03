c956324615b578ecb4dfbd0d7ce080f22a9f3de3
Burton Rheutan <rheutan7@gmail.com>
2022-06-02 23:22:23 -0500

    Update MongoDB connection config

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,8 +1,9 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = process.env.MONGO_URI
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 })
 
 var _db;
