47f05c07f4b5de98129d2c28d79b4866ce24fba6
Burton Rheutan <rheutan7@gmail.com>
2023-10-11 21:51:01 -0500

    Store setting in cookie to retain across refresh
    Adding react-cookies library to ease adding and getting cookie values.
    Set the value of the darkMode setting in a cookie using hooks when the
    value changes. Also, added functionality to read the cookie when the
    settings load when the app loads

--- src/features/app/appSlice.js
+++ src/features/app/appSlice.js
@@ -1,5 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit"
 import { apiSlice } from "../api/apiSlice"
+import { Cookies } from "react-cookie"
+
+const cookies = new Cookies()
+
+const cookieNames = {
+    darkMode: 'darkMode'
+}
 
 const appSlice = createSlice({
     name: 'app',
@@ -17,8 +24,12 @@ const appSlice = createSlice({
         }
     },
     extraReducers(builder) {
+        builder.addMatcher(appSlice.actions.toggleDarkMode.match, (state) => {
+            cookies.set(cookieNames.darkMode, state.darkMode, { page: '/', sameSite: 'lax' })
+        })
         builder.addMatcher(apiSlice.endpoints.getSettings.matchFulfilled, (state, { payload }) => {
             state.authDisabled = payload.authDisabled
+            state.darkMode = cookies.get(cookieNames.darkMode)
         })
     }
 })
