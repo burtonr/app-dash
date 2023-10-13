53a20cd4eb253fdd9e7735387f4156603e3701b6
Burton Rheutan <rheutan7@gmail.com>
2023-10-12 21:36:57 -0500

    Use localStorage and remove react-cookie

--- src/features/app/appSlice.js
+++ src/features/app/appSlice.js
@@ -1,10 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit"
 import { apiSlice } from "../api/apiSlice"
-import { Cookies } from "react-cookie"
 
-const cookies = new Cookies()
-
-const cookieNames = {
+const storageNames = {
     darkMode: 'darkMode'
 }
 
@@ -25,11 +22,11 @@ const appSlice = createSlice({
     },
     extraReducers(builder) {
         builder.addMatcher(appSlice.actions.toggleDarkMode.match, (state) => {
-            cookies.set(cookieNames.darkMode, state.darkMode, { page: '/', sameSite: 'lax' })
+            localStorage.setItem(storageNames.darkMode, state.darkMode)
         })
         builder.addMatcher(apiSlice.endpoints.getSettings.matchFulfilled, (state, { payload }) => {
             state.authDisabled = payload.authDisabled
-            state.darkMode = cookies.get(cookieNames.darkMode)
+            state.darkMode = localStorage.getItem(storageNames.darkMode)
         })
     }
 })
